9b9f163603d3fbb7d135c5aee4698757a61b1178
Leonardo <leonardohyx@gmail.com>
2022-07-18 10:02:13 +0800

    update url prefix

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ Encore.setOutputPath('./public/assets')
 | relative from the "public" directory.
 |
 */
-Encore.setPublicPath('/assets')
+Encore.setPublicPath('assets')
 
 /*
 |--------------------------------------------------------------------------
